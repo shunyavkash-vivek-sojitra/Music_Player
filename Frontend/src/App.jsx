5ae702bf5fcdca084f708d71682c68b75f5bfed3
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-03-28 18:23:07 +0530

    Fix: Loader Component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchSongs } from "./services/actions.js";
 import Navbar from "./components/Navbar.jsx";
@@ -8,23 +8,27 @@ import Loader from "./components/Loader.jsx";
 
 const App = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     dispatch(fetchSongs());
   }, [dispatch]);
 
   useEffect(() => {
-    // const loaderTimeOut = setTimeout(() => {
-    <Loader />;
-    // }, 3000);
+    const loaderTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
 
-    // clearTimeout(loaderTimeOut);
+    return () => clearTimeout(loaderTimeout);
   }, []);
 
   return (
     <div>
-      <Navbar />
-      <Home />
+      {loading && <Loader />}
+      <div style={{ display: loading ? "none" : "block" }}>
+        <Navbar />
+        <Home />
+      </div>
     </div>
   );
 };
